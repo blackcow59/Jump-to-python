66b10227bc85d96480f80ba15da27893998133ca
blackcow59 <choi8695@gmail.com>
2021-01-17 01:57:43 +0900

    ~2020-01-16분량

--- 4장 연습문제.py
+++ 4장 연습문제.py
@@ -10,6 +10,20 @@ is_odd(3)
 is_odd(30)
 
 
+# 풀이
+def is_odd(number):
+     if number % 2 == 1:   # 2로 나누었을 때 나머지가 1이면 홀수이다.
+         return True
+     else:
+         return False
+
+is_odd(3)
+is_odd(4)
+
+
+# 람다와 조건부 표현식을 사용하면 다음과 같이 간단하게도 만들 수 있다.
+is_odd = lambda x : True if x % 2 == 1 else False
+is_odd(3)
 
 ### 2. 입력으로 들어오는 모든 수의 평균 값을 계산해 주는 함수를 작성해 보자.(단 입력으로 들어오는 수의 개수는 정해져 있지 않다.)
 
@@ -22,6 +36,15 @@ def mean(*args ):
 
 mean(50, 30, 40)
 
+# 해설
+def avg_numbers(*args):   # 입력 개수에 상관없이 사용하기 위해 *args를 사용
+     result = 0
+     for i in args:
+         result += i
+     return result / len(args)
+
+avg_numbers(1, 2)
+avg_numbers(1,2,3,4,5)
 
 
 ### 3. 다음은 두 개의 숫자를 입력받아 더하여 돌려주는 프로그램이다.
@@ -41,7 +64,12 @@ input2 = int(input("두번째 숫자를 입력하세요:"))
 total = input1 + input2
 print('두수의 합은 %d 입니다.' % total)
 
+# 해설
+input1 = input("첫 번째 숫자를 입력하세요:")
+input2 = input("두 번째 숫자를 입력하세요:")
 
+total = int(input1) + int(input2)      # 입력은 항상 문자열이므로 숫자로 바꾸어 주어야 한다.
+print("두수의 합은 %s 입니다" % total)
 
 ### 4. 다음 중 출력 결과가 다른 것 한 개를 골라 보자.
 
@@ -74,6 +102,25 @@ line = f2.readline()
 print(line)
 f2.close()
 
+# 해설
+# 문제의 예와 같이 파일을 닫지 않은 상태에서 다시 열면 파일에 저장한 데이터를 읽을 수 없다. 따라서 열린 파일 객체를 close로 닫아준 후 
+# 다시 열어서 파일의 내용을 읽어야 한다.
+f1 = open("test.txt", 'w')
+f1.write("Life is too short!")
+f1.close() # 열린 파일 객체를 닫는다.
+
+f2 = open("test.txt", 'r')
+print(f2.read())
+f2.close()
+
+# 또는 다음과 같이 close를 명시적으로 할 필요가 없는 with구문을 사용한다.
+with open("test.txt", 'w') as f1:
+    f1.write("Life is too short! ")
+
+with open("test.txt", 'r') as f2:
+    print(f2.read())
+
+
 
 
 ### 6. 사용자의 입력을 파일(test.txt)에 저장하는 프로그램을 작성해 보자.(단 프로그램을 다시 실행하더라도 기존에 작성한 내용을 유지하고 
@@ -88,6 +135,14 @@ def inputsave(data):
 
 inputsave("R is better than Python")
 
+# 해설
+# 기존 내용을 유지하고 새로운 내용을 덧붙이기 위해서 다음과 같이 'a' 모드를 사용해야 한다.
+user_input = input("저장할 내용을 입력하세요:")
+f = open('test.txt', 'a')   # 내용을 추가하기 위해서 'a'를 사용
+f.write(user_input)
+f.write("\n")               # 입력된 내용을 줄 단위로 구분하기 위해 줄 바꿈 문자 삽입
+f.close()
+
 
 
 ### 7. 다음과 같은 내용을 지닌 파일 test.txt가 있다. 이 파일의 내용 중 "java"라는 문자열을 "python"으로 바꾸어서 저장해 보자.
@@ -98,12 +153,22 @@ f.write("Life is too short\n you need java")
 f.close()
 
 f1 = open("test.txt", 'r')
-lines = f1.readlines()
-for line in lines :
-    print(line)
+line = f1.read()
+print(line)
 f1.close()
 
-f2 = open("test.txt", 'a')
-line = f2.readlines()
+f2 = open("test.txt", 'w')
 f2.write(line.replace('java', 'python'))
 f2.close()
+
+# 해설
+# 파일을 모두 읽은 후에 문자열의 replace 함수를 사용하여 java라는 문자열을 python으로 변경한 다음 저장한다.
+f = open('test.txt', 'r')
+body = f.read()
+f.close()
+
+body = body.replace('java', 'python')
+
+f = open('test.txt', 'w')
+f.write(body)
+f.close()
